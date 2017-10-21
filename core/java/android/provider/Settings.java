3ebca28723ddf2625c621a5dc51081bb13dc5000
ezio84 <brabus84@gmail.com>
2017-10-21 14:46:24 +0200

    Slim Recents 3.0 [1/3]
    Refactored/improved everything.
    Slim Recents won't never die.
    
    http://i.imgur.com/PO3RSeg.gif
    
    Change-Id: I9deb0412e2421df2ac2755f2111a3cec70a7d2c4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4063,12 +4063,6 @@ public final class Settings {
         public static final String USE_SLIM_RECENTS = "use_slim_recents";
 
         /**
-         * Whether to only show actually running tasks
-         * @hide
-         */
-        public static final String RECENT_SHOW_RUNNING_TASKS = "show_running_tasks";
-
-        /**
          * Amount of apps to show in recents
          * @hide
          */
@@ -4101,13 +4095,6 @@ public final class Settings {
         public static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
 
         /**
-         * Recent panel: Show topmost task
-         *
-         * @hide
-         */
-        public static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
-
-        /**
          * Recent panel background color
          *
          * @hide
@@ -4120,13 +4107,6 @@ public final class Settings {
          * @hide
          */
         public static final String RECENT_CARD_BG_COLOR = "recent_card_bg_color";
-
-        /**
-         * Recent card text color
-         *
-         * @hide
-         */
-        public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
         /**
          * Slim recents AICPfied empty recents drawable
@@ -4224,6 +4204,12 @@ public final class Settings {
          * @hide
          */
         public static final String SLIM_RECENTS_CORNER_RADIUS = "slim_recents_corner_radius";
+
+        /**
+         * Apps to blacklist in Slim Recents
+         * @hide
+         */
+        public static final String SLIM_RECENTS_BLACKLIST_VALUES = "slim_recents_blacklist_values";
 
         /**
          * --- NucleaRom System settings end
